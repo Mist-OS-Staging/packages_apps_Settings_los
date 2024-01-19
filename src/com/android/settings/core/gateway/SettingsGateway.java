8278c0ae238058eae7dc8f7f9dd2599aac33e23c
Ido Ben-Hur <idoybh2@gmail.com>
2024-01-19 05:43:40 +0200

    Settings: Allow customizing volume stream steps [2/2]
    only media, calls and alarms seem fit
    the rest are pretty much set and forget and don't need vol button granularity
    
    Change-Id: I6ce6fa79df7b94ae84912945f6129048895067b2

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -198,6 +198,7 @@ import com.android.settings.security.SecurityAdvancedSettings;
 import com.android.settings.security.SecuritySettings;
 import com.android.settings.shortcut.CreateShortcut;
 import com.android.settings.sound.MediaControlsSettings;
+import com.android.settings.sound.VolumeSteps;
 import com.android.settings.support.SupportDashboardActivity;
 import com.android.settings.system.ResetDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
@@ -422,6 +423,8 @@ public class SettingsGateway {
             ContentProtectionPreferenceFragment.class.getName(),
             ToggleScreenMagnificationPreferenceFragment.class.getName(),
             FlashNotificationsPreferenceFragment.class.getName(),
+            PrivateDnsSettings.class.getName(),
+            VolumeSteps.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
