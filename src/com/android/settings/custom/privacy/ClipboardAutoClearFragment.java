TheParasiteProject/packages_apps_Settings

Settings: Expose clipboard auto clear setting [2/2]
[someone5678]
* Adapt to current project
* Use Settings instead of DeviceConfig as GMS don't likes it
* Add switch for clipboard auto clear
* Import resources from CalyxOS and crDroid Android and adapt it
* Update summary setting logic

Ref:
https://gitlab.com/CalyxOS/platform_packages_apps_Settings/-/commit/72db57c966dd996cb7a9106e52a6416c589df92d
https://github.com/crdroidandroid/android_packages_apps_Settings/commit/48e00e2b812db5ea85253edaced9b1fc48db0efd
https://github.com/crdroidandroid/android_packages_apps_crDroidSettings/commit/33c49aa70c9250d64b284af6ee46d5dad618497c
https://github.com/crdroidandroid/android_packages_apps_crDroidSettings/commit/bc81eea9cce7d964b2a3f64ae318cd8d0c840adb

Issue: calyxos#2208
Change-Id: Ie101177aba90ea085d83c0cb641ffed447cceecd
Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

src/com/android/settings/custom/privacy/ClipboardAutoClearFragment.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2024 TheParasiteProject
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.custom.privacy;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
+
+@SearchIndexable
+public class ClipboardAutoClearFragment extends SettingsPreferenceFragment {
+
+    public static final String TAG = "ClipboardAutoClearFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.clipboard_auto_clear_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.clipboard_auto_clear_settings) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    return keys;
+                }
+            };
+}

Mist-OS-Staging/packages_apps_Settings_los

src/com/android/settings/custom/privacy/ClipboardAutoClearFragment.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2024 TheParasiteProject
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.custom.privacy;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
+
+@SearchIndexable
+public class ClipboardAutoClearFragment extends SettingsPreferenceFragment {
+
+    public static final String TAG = "ClipboardAutoClearFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.clipboard_auto_clear_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.MIST;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.clipboard_auto_clear_settings) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    return keys;
+                }
+            };
+}