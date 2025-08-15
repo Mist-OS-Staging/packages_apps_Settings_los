Evolution-X/packages_apps_Settings

Settings: Do not hide Now Playing for Pixel devices

src/com/android/settings/notification/SoundSettings.java
@@ -51,6 +51,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.evolution.settings.utils.DeviceUtils;
+
 @SearchIndexable
 public class SoundSettings extends DashboardFragment implements OnActivityResultListener {
     private static final String TAG = "SoundSettings";
@@ -116,7 +118,9 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         if (phoneRingTonePreference != null && openPhoneRingtonePicker) {
             onPreferenceTreeClick(phoneRingTonePreference);
         }
-        updateAmbientMusicPref();
+        if (!DeviceUtils.isCurrentlySupportedPixel()) {
+            updateAmbientMusicPref();
+        }
     }
 
     private void updateAmbientMusicPref() {

src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -51,6 +51,8 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.evolution.settings.utils.DeviceUtils;
+
 /**
  * Settings screen for lock screen preference
  */
@@ -95,7 +97,9 @@ public class LockscreenDashboardFragment extends DashboardFragment
                 R.string.locked_work_profile_notification_title);
         replaceEnterpriseStringTitle("security_setting_lock_screen_notif_work_header",
                 WORK_PROFILE_NOTIFICATIONS_SECTION_HEADER, R.string.profile_section_header);
-        updateAmbientMusicPref();
+        if (!DeviceUtils.isCurrentlySupportedPixel()) {
+            updateAmbientMusicPref();
+        }
     }
 
     private void updateAmbientMusicPref() {

Mist-OS-Staging/packages_apps_Settings_los

src/com/android/settings/notification/SoundSettings.java
@@ -54,6 +54,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.mist.settings.utils.DeviceUtils;
+
 @SearchIndexable
 public class SoundSettings extends DashboardFragment implements OnActivityResultListener {
     private static final String TAG = "SoundSettings";
@@ -122,7 +124,9 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             }
             return null;
         });
-        updateAmbientMusicPref();
+        if (!DeviceUtils.isCurrentlySupportedPixel()) {
+            updateAmbientMusicPref();
+        }
     }
 
     private void updateAmbientMusicPref() {

src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -51,6 +51,8 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mist.settings.utils.DeviceUtils;
+
 /**
  * Settings screen for lock screen preference
  */
@@ -96,7 +98,9 @@ public class LockscreenDashboardFragment extends DashboardFragment
                 R.string.locked_work_profile_notification_title);
         replaceEnterpriseStringTitle("security_setting_lock_screen_notif_work_header",
                 WORK_PROFILE_NOTIFICATIONS_SECTION_HEADER, R.string.profile_section_header);
-        updateAmbientMusicPref();
+        if (!DeviceUtils.isCurrentlySupportedPixel()) {
+            updateAmbientMusicPref();
+        }
     }
 
     private void updateAmbientMusicPref() {